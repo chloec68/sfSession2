dd68a4073fd78d4555cab10623e4c6f37ee6076e
Chloé Cilia <chloe.cilia68@gmail.com>
2025-01-30 12:38:27 +0100

    add trainee method

--- src/Controller/TraineeController.php
+++ src/Controller/TraineeController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Trainee;
+use App\Form\TraineeType;
 use App\Repository\TraineeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -24,6 +27,41 @@ final class TraineeController extends AbstractController
         ]);
     }
 
+    #[Route('/trainee/add', name: 'add_trainee')]
+    #[Route('/trainee/{id}/update', name: 'update_trainee')]
+    public function AddUpdateTrainee(Request $request, EntityManagerInterface $entityManager, ?Trainee $trainee = null): Response
+    {
+        if(!$trainee){
+            $trainee = new Trainee();
+        }
+
+        $form = $this->createForm(TraineeType::class, $trainee);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $trainee = $form->getData();
+            $entityManager->persist($trainee);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_trainee');
+        }
+
+        return $this->render('trainee/new-update-trainee.html.twig', [
+            'formNewTrainee' => $form,
+            'edit' => $trainee->getId()
+        ]);
+
+    }
+
+    #[Route('/trainee/{id}/delete', name: 'delete_trainee')]
+    public function deleteTrainee(Trainee $trainee, EntityManagerInterface $entityManager, int $id)
+    {
+        $entityManager->remove($trainee);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_trainee');
+    }
+
     #[Route('/trainee/{id}', name: 'detail_trainee')]
     public function detailTrainee(Trainee $trainee): Response
     {
@@ -31,5 +69,6 @@ final class TraineeController extends AbstractController
             'trainee'=>$trainee
         ]);
     }
+
 }
 
---
+++ src/Form/TraineeType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use App\Entity\Trainee;
+use App\Form\FormNewTraineeType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TraineeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('dateOfBirth', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('email')
+            ->add('address')
+            ->add('postcode')
+            ->add('city')
+            ->add('sessions', EntityType::class, [
+                'class' => Session::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Trainee::class,
+        ]);
+    }
+}
